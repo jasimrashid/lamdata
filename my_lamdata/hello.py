80ca96671d33a035db0e156c002e0acf259df54c
Jasim Rashid <jasim.rashid@gmail.com>
2020-06-02 04:41:18 +0530

    prepare to distribute package

--- my_lamdata/hello.py
+++ my_lamdata/hello.py
@@ -1,6 +1,15 @@
+from pandas import DataFrame
 
 from my_lamdata.my_mod import enlarge
 
 print("HELLO")
 
 print(enlarge(8))
+
+df = DataFrame({"state": ["CT", "CO", "CA", "TX"]})
+print(df.head())
+
+# print("-----------------")
+# x = 5
+# print("NUMBER", x)
+# print("ENLARGED NUMBER", enlarge(x)) # invoking our function!!
---
+++ setup.py
@@ -0,0 +1,20 @@
+# setup.py file
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="my-package-name", # the name that you will install via pip
+    version="1.0",
+    author="Jasim Rashid",
+    author_email="jasim.rashid@gmail.com",
+    description="My first package installation",
+    long_description=long_description,
+    long_description_content_type="text/markdown", # required if using a md file for long desc
+    #license="MIT",
+    url="https://github.com/jasimrashid/lamdata",
+    #keywords="",
+    packages=find_packages() # ["my_lambdata"]
+)
